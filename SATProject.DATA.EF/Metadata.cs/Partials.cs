8ef27210c22f70ec6a2c0808cb85bfa6241df84b
Roan Leffew <roan.leffew@gmail.com>
2022-06-22 10:58:47 -0500

    Partials

--- SATProject.DATA.EF/Metadata.cs/Partials.cs
+++ SATProject.DATA.EF/Metadata.cs/Partials.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SATProject.DATA.EF.Metadata.cs
 {
-    internal class Partials
-    {
-    }
+
+    #region Course
+    [ModelMetadataType(typeof(CourseMetadata))]
+
+    public class CourseMetadata { }
+
+
+
+    #endregion
+
+    #region Enrollment
+
+    [ModelMetadataType(typeof(EnrollmentMetadata))]
+
+    public class EnrollmentMetadata { }
+
+
+    #endregion
+
+    #region ScheduledClass
+
+    [ModelMetadataType(typeof(ScheduledClassMetadata))]
+
+    public class ScheduledClassMetadata { }
+
+    #endregion
+
+    #region ScheduledClassStatus
+
+    [ModelMetadataType(typeof(ScheduledClassStatusMetadata))]
+
+    public class ScheduledClassStatusMetadata { }
+
+    #endregion
+
+    #region Student
+
+    [ModelMetadataType(typeof(StudentMetadata))]
+
+    public class StudentMetadata { }
+
+
+    #endregion
+
+    #region StudentStatus
+
+    [ModelMetadataType(typeof(StudentStatusMetadata))]
+
+    public class SentStudentMetadata { }
+
+
+
+    #endregion
+
+
+
 }
