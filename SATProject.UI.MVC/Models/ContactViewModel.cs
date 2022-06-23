0855781d2f28bbd474e3469dca04c3e84b87f768
Roan Leffew <roan.leffew@gmail.com>
2022-06-23 15:34:10 -0500

    Added styling and contact form

---
+++ SATProject.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SATProject.UI.MVC.Models
+
+
+{
+    public class ContactViewModel
+    {
+
+        [Required(ErrorMessage = "*Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Subject is required ")]
+
+        public string Subject
+        { get; set; }
+
+        [Required(ErrorMessage = "*Message is required")]
+        [DataType(DataType.MultilineText)]
+
+        public string Message { get; set; }
+
+
+    }
+}
